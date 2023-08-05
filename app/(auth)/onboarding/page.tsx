bca89de401e76c61ed5998029fda746902fd013f
Yassar021 <yassar.tkj114@gmail.com>
2023-08-06 01:00:00 +0800

    feat: onBoarding page functionality

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,7 +1,30 @@
+import AccountProfile from "@/components/forms/AccountProfile";
+import { currentUser } from "@clerk/nextjs";
+
 async function Page() {
+  const user = await currentUser();
+
+  const userInfo = {};
+
+  const userData = {
+    id: user?.id,
+    objectId: userInfo?._id,
+    username: userInfo?.username || user?.username,
+    name: userInfo?.name || user?.firstName || "",
+    bio: userInfo?.bio || "",
+    image: userInfo?.image || user?.imageUrl,
+  };
+
   return(
-    <main>
-      <h1 className="head-text">onboarding</h1>
+    <main className="flex-col justify-start max-w-3xl px-10 py-20 mx-auto flex-">
+      <h1 className="head-text">Onboarding</h1>
+      <p className="mt-3 text-base-regular text-light-2">
+        Complete your profile now to use Threds
+      </p>
+
+      <section className="p-10 mt-9 bg-dark-2">
+        <AccountProfile user={userData} btnTitle={'Continue'} />
+      </section>
     </main>
   )
 }
